26636877fd2a0dce552e09ea4275affcfe2fde47
Rodolfo Santos <rodolfodossantos29@gmail.com>
2021-12-07 21:07:56 -0300

    🎲 Adjustment product_branch table

---
+++ database/migrations/2021_12_07_231638_adjustment_product_branches.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AdjustmentProductBranches extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up() {
+    Schema::create('branches', function (Blueprint $table) {
+      $table->id();
+      $table->string('branch', 30);
+      $table->timestamps();
+    });
+
+    Schema::create('product_branches', function (Blueprint $table) {
+      $table->id();
+      $table->unsignedBigInteger('branch_id');
+      $table->unsignedBigInteger('product_id');
+      $table->float('sell_price', 8, 2);
+      $table->integer('min_stock');
+      $table->integer('max_stock');
+      $table->timestamps();
+
+      $table->foreign('branch_id')->references('id')->on('branches');
+      $table->foreign('product_id')->references('id')->on('products');
+    });
+
+    Schema::table('products', function (Blueprint $table) {
+      $table->dropColumn(['sell_price', 'min_stock', 'max_stock']);
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down() {
+    Schema::table('products', function (Blueprint $table) {
+      $table->float('sell_price', 8, 2);
+      $table->integer('min_stock');
+      $table->integer('max_stock');
+    });
+
+    Schema::dropIfExists('product_branches');
+
+    Schema::dropIfExists('branches');
+  }
+}
