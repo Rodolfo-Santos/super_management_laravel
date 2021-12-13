2ca2a4696f8f79534ddbf8068cae9b3b7a8ff164
Rodolfo Santos <rodolfodossantos29@gmail.com>
2021-12-13 20:55:07 -0300

    🎲 Persisting data

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContact;
 
 class ContactController extends Controller {
-  public function contact() {
+  public function contact(Request $request) {
+    $contact = new SiteContact();
+    $contact->create($request->all());
+
     return view('site.contact');
   }
 }
