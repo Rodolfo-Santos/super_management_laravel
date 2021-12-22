c8c0818cde37803106dec326ed365fd8b84a08cc
Rodolfo Santos <rodolfodossantos29@gmail.com>
2021-12-21 22:05:34 -0300

    🔒 Improving validation messages

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -14,13 +14,20 @@ class ContactController extends Controller {
   }
 
   public function save(Request $request) {
-    $request->validate([
+    $rules = [
       'name'  => 'required|min:3|max:40',
       'phone' => 'required',
       'email' => 'email|required|unique:site_contacts',
       'contact_reason_id' => 'required',
       'message' => 'required|max:2000',
-    ]);
+    ];
+
+    $feedback = [
+      'name.required' => 'Insert your name, please.',
+      'unique' => 'The :attribute already exists.'
+    ];
+
+    $request->validate($rules, $feedback);
 
     SiteContact::create($request->all());
   }
