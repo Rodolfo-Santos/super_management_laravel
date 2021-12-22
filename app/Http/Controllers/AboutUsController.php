6ac157fa13f4b94f1c3a194c5896bd5a3c6b61a0
Rodolfo Santos <rodolfodossantos29@gmail.com>
2021-12-22 20:54:02 -0300

    🌉 Implementing Middleware within the Controller

--- app/Http/Controllers/AboutUsController.php
+++ app/Http/Controllers/AboutUsController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Middleware\LogAccessMiddleware;
 
 class AboutUsController extends Controller {
+  public function __construct() {
+    $this->middleware(LogAccessMiddleware::class);
+  }
+
   public function aboutUs() {
     return view('site.about_us');
   }
