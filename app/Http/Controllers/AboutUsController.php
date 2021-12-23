0f929a921bdbd2a85c24ce7d7e34ff8516e45d6a
Rodolfo Santos <rodolfodossantos29@gmail.com>
2021-12-22 21:08:11 -0300

    🌉 Implementing Middleware on Kernel (web group)

--- app/Http/Controllers/AboutUsController.php
+++ app/Http/Controllers/AboutUsController.php
@@ -2,13 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Middleware\LogAccessMiddleware;
-
 class AboutUsController extends Controller {
-  public function __construct() {
-    $this->middleware(LogAccessMiddleware::class);
-  }
-
   public function aboutUs() {
     return view('site.about_us');
   }
--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,8 @@
 use App\Http\Middleware\LogAccessMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(LogAccessMiddleware::class)
-  ->get('/', 'PrincipalController@principal')
-  ->name('site.index');
-
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/about_us', 'AboutUsController@aboutUs')->name('site.about_us');
-
 Route::get('/contact', 'ContactController@contact')->name('site.contact');
 Route::post('/contact', 'ContactController@save')->name('site.contact');
 
