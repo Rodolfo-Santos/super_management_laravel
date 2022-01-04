31d214a65fb2b06aa8dd39d24d419e92da124738
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-01-04 20:34:41 -0300

    🔑 Create Authentication Base

---
+++ resources/views/site/login.blade.php
@@ -0,0 +1,38 @@
+@extends('site.layouts.basic')
+@section('title', $title)
+
+@section('content')
+  <div class="page-content">
+    <div class="page-title">
+      <h1>Contact Us</h1>
+    </div>
+
+    <div class="page-information">
+      <form action="{{ route('site.login') }}" method="POST" style="width: 30%; margin: 0 auto;">
+        @csrf
+        <input name="user" type="text" placeholder="User" class="black-border">
+        <input name="password" type="text" placeholder="Password" class="black-border">
+        <button type="submit" class="black-border">Acessar</button>
+      </form>
+    </div>
+  </div>
+
+  <div class="footer">
+    <div class="social-networks">
+      <h2>Social Networks</h2>
+      <img src="{{ asset('img/facebook.png') }}">
+      <img src="{{ asset('img/linkedin.png') }}">
+      <img src={{ asset('img/youtube.png') }}">
+    </div>
+    <div class="area-contact">
+      <h2>Contact</h2>
+      <span>(11) 3333-4444</span>
+      <br>
+      <span>supergestao@dominio.com.br</span>
+    </div>
+    <div class="location">
+      <h2>Location</h2>
+      <img src={{ asset('img/mapa.png') }}>
+    </div>
+  </div>
+@endsection
