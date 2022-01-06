a576749fc171cf139b241592831d72880350d28a
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-01-06 20:08:34 -0300

    🎮 Add list and add methods

--- app/Http/Controllers/SuppliersController.php
+++ app/Http/Controllers/SuppliersController.php
@@ -4,6 +4,14 @@ namespace App\Http\Controllers;
 
 class SuppliersController extends Controller {
   public function index() {
-    return view('app.supplier');
+    return view('app.supplier.index');
+  }
+
+  public function list() {
+    return view('app.supplier.list');
+  }
+
+  public function add() {
+    return view('app.supplier.add');
   }
 }
