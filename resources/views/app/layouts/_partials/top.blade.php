0d956f2f624565e5ed3a4fd8553733ed9e25bdcb
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-01-05 21:48:05 -0300

    💻 Add App Layout

---
+++ resources/views/app/layouts/_partials/top.blade.php
@@ -0,0 +1,16 @@
+<div class="top">
+
+  <div class="logo">
+    <img src="{{ asset('img/logo.png') }}">
+  </div>
+
+  <div class="menu">
+    <ul>
+      <li><a href="{{ route('app.home') }}">Home</a></li>
+      <li><a href="{{ route('app.customer') }}">Customer</a></li>
+      <li><a href="{{ route('app.supplier') }}">Supplier</a></li>
+      <li><a href="{{ route('app.product') }}">Product</a></li>
+      <li><a href="{{ route('app.exit') }}">Exit</a></li>
+    </ul>
+  </div>
+</div>
