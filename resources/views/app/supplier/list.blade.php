38ee00e92a7aebc014216bad96a13b603937fe3f
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-01-07 20:13:41 -0300

    💻 Create List Table

--- resources/views/app/supplier/list.blade.php
+++ resources/views/app/supplier/list.blade.php
@@ -11,8 +11,32 @@
   @include('app.supplier._partials.menu')
 
   <div class="page-info">
-    <div style="width: 30%; margin: 0 auto;">
-      --- List ---
+    <div style="width: 90%; margin: 0 auto;">
+      <table border="1" width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Site</th>
+            <th>UF</th>
+            <th>E-mail</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($suppliers as $supplier)
+            <tr>
+              <td>{{ $supplier->name}}</td>
+              <td>{{ $supplier->site}}</td>
+              <td>{{ $supplier->uf}}</td>
+              <td>{{ $supplier->email}}</td>
+              <td>Edit</td>
+              <td>Delete</td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+          
     </div>
   </div>
 </div>
